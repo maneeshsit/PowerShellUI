33f18ad5a4abd089dfbd882ebc0f07edb5e182ee
maneeshsit <contact@digx.tech>
2025-01-27 19:38:48 +0530

    Exploring UIs in PowerShell
    Exploring UIs in PowerShell

---
+++ UI.ps1
@@ -0,0 +1,10 @@
+﻿[void][reflection.assembly]::LoadWithPartialName("system.windows.forms")
+$form.text=new-object windows.forms.form
+$form.text="My first form"
+$button=new-object windows.form.button
+$button.text="push me!"
+$button.dock="fill"
+$button.add_click({$form.close()})
+$form.controls.add($button)
+$form.add_shown({$form.activate()})
+$form.showdialog()
